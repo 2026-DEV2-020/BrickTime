b4847c7dc3224efd994182fe5f6ff2a70f5d1011
2026-DEV2-020 <shadarayhan@gmail.com>
2026-01-23 05:12:35 +0200

    Initialize ViewModel with midnight state

--- BrickTime/ViewModel/BrickTimeViewModel.swift
+++ BrickTime/ViewModel/BrickTimeViewModel.swift
@@ -10,13 +10,16 @@ import Foundation
 final class BrickTimeViewModel {
     
     // MARK: - private properties
-    private(set) var time: BrickTimeState?
-    private(set) var formattedTime: String?
-    
+    private(set) var time: BrickTimeState
+    private(set) var formattedTime: String
     private let tranformer: BrickTimeTransforming
-
+    
     // MARK: - init
     init(tranformer: BrickTimeTransforming) {
         self.tranformer = tranformer
+        
+        let initialTime = tranformer.transforme(hour: 0, minutes: 0, seconds: 0)
+        self.time = initialTime
+        self.formattedTime = "00:00:00"
     }
 }
