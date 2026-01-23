4a17db0d0ea7ba6b3903d98ea56f1850cbd72e66
2026-DEV2-020 <shadarayhan@gmail.com>
2026-01-23 03:55:28 +0200

    Support hour rows in BrickTimeTransformer

--- BrickTime/Model/BrickTimeTransformer.swift
+++ BrickTime/Model/BrickTimeTransformer.swift
@@ -21,11 +21,16 @@ struct BrickTimeTransformer: BrickTimeTransforming {
     // MARK: - public funcs
     public func transforme(hour: Int, minutes: Int, seconds: Int) -> BrickTimeState {
         BrickTimeState (
-            seconds: true,
-            fiveHours: [.off, .off, .off, .off],
-            singleHours: [.off, .off, .off, .off],
+            seconds: seconds % 2 == 0,
+            fiveHours: row(count: hour / 5, total: 4, color: .red),
+            singleHours: row(count: hour % 5, total: 4, color: .red),
             fiveMinutes: Array(repeating: .off, count: 11),
             singleMinutes: [.off, .off, .off, .off]
         )
+    }
+    
+    // MARK: - private funcs
+    private func row(count: Int, total: Int, color: Light) -> [Light] {
+        (0..<total).map { $0 < count ? color : .off }
     }
 }
